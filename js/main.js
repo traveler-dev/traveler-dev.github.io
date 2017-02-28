99e1a6e4b3867d8ffc7f917d16e2f45adb970c8e
Daniel Carlson <dancarl758@gmail.com>
2017-02-28 14:47:22 +0100

    Adding code for email

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 /**
  * Created by daniel on 2/22/17.
  */
+
+$(document).ready(function() {
+    $('#errorMsg').hide();
+    $('#successMsg').hide();
+});
+
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: 4.0510564, lng: 9.7678687};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
         center: uluru
@@ -11,4 +17,30 @@ function initMap() {
         position: uluru,
         map: map
     });
+}
+
+function sendMail() {
+    var senderEmail = $("#exampleInputEmail1").val();
+    var subject = $("#subject").val();
+
+    if ($('textarea#exampleTextarea') != undefined) {
+        var message = $('textarea#exampleTextarea').val();
+    }
+
+    if(senderEmail == null || subject == null || message == null){
+        $('#errorMsg').show();
+    }
+
+    Email.send(
+        senderEmail,
+        "infos@traveler.cm",
+        subject,
+        message,
+        {token: "3d005761-1196-492d-bb47-a2098ff2c51a"});
+    setTimeout(function(){
+        $('#successMsg').show();
+        setTimeout(function() {
+            location.reload();
+        }, 5000);
+    }, 1000);
 }
